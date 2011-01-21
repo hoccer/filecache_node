93a7f09dc88a17c5f22b721db771475ed853d7b4
Robert Palmer <hi@robertpalmer.de>
2011-01-21 12:39:52 +0100

    nstore for hashes

--- filecache.js
+++ filecache.js
@@ -1,11 +1,11 @@
 var http = require('http'),
     fs = require('fs'),
     url = require('url');
+var nstore = require('nstore');
 
 var inCreatedDirectory = function(dicts, callback) {
   var createDirRecursiv = function(dictionaries, path, callback) {
     if (dictionaries.length == 0) {
-      console.log(callback);
       callback.call(this, path);
       return;
     }
@@ -13,7 +13,7 @@ var inCreatedDirectory = function(dicts, callback) {
     path = (path || "") + dictionaries[0] + "/";
     fs.stat(path, function(err, data) {
       if (err && err.errno == 2) {
-        fs.mkdir(path, 0777, function() {
+        fs.mkdir(path, 7777, function() {
           createDirRecursiv(dictionaries.slice(1), path, callback);
         });
       } else {
@@ -27,34 +27,34 @@ var inCreatedDirectory = function(dicts, callback) {
 
 var splittedUuid = function(uuid) {
   var compact_uuid = uuid.replace(/[-\/]/g, "");
-  return [compact_uuid.slice(0, 6), compact_uuid.slice(6)];
+  return [compact_uuid.slice(0, 4), compact_uuid.slice(4)];
 }
 
 
 var saveToFile = function(req, res) {
-  var writeStream, buffer;
-  var ended = false;
-  
+  var dataStream, buffer, ended = false;
   var uuid = url.parse(req.url).pathname;
-  
+
   var endHeader = function() {
+      dataStream.end();
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Hello World\n');
   }
   
+  files.save(uuid, { "expires", res.params["expires_at"] });
+  
   inCreatedDirectory(splittedUuid(uuid), function(path) {
-    var writeStream = fs.createWriteStream(path + "data");
-    writeStream.write(buffer);
+    dataStream = fs.createWriteStream(path + "data");
+    dataStream.write(buffer);
     
     if (ended) {
-      writeStream.end();
       endHeader();
     }
   });
   
   req.on('data', function(chunk) {
-    if (writeStream) {
-      writeStream.write(chunk);
+    if (dataStream) {
+      dataStream.write(chunk);
     } else {
       buffer = chunk;
     }
@@ -62,8 +62,7 @@ var saveToFile = function(req, res) {
 
   req.on('end', function() {
     ended = true;
-    if (writeStream) {
-      writeStream.end();
+    if (dataStream) {
       endHeader();
     } 
   });  
@@ -74,24 +73,39 @@ var loadFile = function(req, res) {
   
   var path = splittedUuid(uuid).concat(["data"]).join("/");
   var readStream = fs.createReadStream(path);
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
   readStream.on('data', function(data) {
-    console.log(data);
+    res.write(data)
   });
+
   readStream.on('end', function() {
-    console.log("end");
+    res.end();
   });
-  
-  
 }
 
+// var loadFile = function(req, res) {
+//   with_file(path, function(file) {
+//     if (!file.exists || file.expired) {
+//       res.writeHead(404);
+//       res.end();
+//       return;
+//     }
+//     
+//     file.streamTo(res);
+//   });
+// }
 
+var files = nStore.new('data/files', function() {
+  http.createServer(function (req, res) {
+    if (req.method == "PUT") {
+      saveToFile(req, res);
+    } else if (req.method == "GET") {
+      loadFile(req, res);
+    }
+  }).listen(8124, "0.0.0.0");
 
-http.createServer(function (req, res) {
-  if (req.method == "PUT") {
-    saveToFile(req, res);
-  } else if (req.method == "GET") {
-    loadFile(req, res);
-  }
-}).listen(8124, "0.0.0.0");
-
-console.log('Server running at http://127.0.0.1:8124/');
+  console.log('Server running at http://127.0.0.1:8124/');
+  
+});

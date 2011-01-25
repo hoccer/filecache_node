773742db15cb21b42c37709fdc2ade6a442314a6
Robert Palmer <hi@robertpalmer.de>
2011-01-25 17:55:13 +0100

    cleanup and splitting

---
+++ authenticate.js
@@ -0,0 +1,80 @@
+var mongo = require('mongodb'),
+   crypto = require('crypto'),
+   http = require('http');
+  
+function toArray(obj) {
+  var len = obj.length
+    , arr = new Array(len);
+  for (var i = 0; i < len; ++i) {
+    arr[i] = obj[i];
+  }
+  return arr;
+}
+
+
+
+http.IncomingMessage.prototype.startBuffering = function() {
+  this._eventBuffer = [];
+  this.on('data', function() {
+    this._eventBuffer.push(['data'].concat(toArray(arguments)));
+  });
+  this.on('end', function() {
+    this._eventBuffer.push(['end'].concat(toArray(arguments)));
+  })  
+}
+
+http.IncomingMessage.prototype.stopBuffering = function() {
+  that = this;
+  this.removeAllListeners('data');
+  this.removeAllListeners('end');
+
+  setTimeout(function(array) {
+    return function() {
+      for (var i = 0; i < array.length; i++) {
+        that.emit.apply(that, array[i]);
+      }
+    }    
+  }(this._eventBuffer), 20);
+
+  this._eventBuffer = null;
+}
+
+exports.authenticate = function() {
+  var db = new mongo.Db('hoccer_development', new mongo.Server("127.0.0.1", 27017));
+  db.open(function(){ console.log("open") });
+  
+  return function(req, res, next) {
+    var reject = function() {
+      res.writeHead(401);
+      res.end("authentification failed");
+    }
+  
+    req.startBuffering();
+    db.collection('accounts', function(err, collection) {
+      collection.findOne({"api_key": "37d4b750fc95012d14a7109add515cd4"}, {}, function(err, account) {
+        if (!account) {
+          reject(); return;
+        } else {
+          var result = req.url.match(/\&signature=(.*)$/);
+          if (result.length < 1) {
+            reject(); return;
+          }
+        
+          var signature = decodeURIComponent(result[1].toString());
+          var url_without_signature = req.url.match(/(.*)\&signature=.*$/)[1];
+        
+          var calculated_hash = crypto.createHmac('sha1', account.shared_secret)
+                                     .update("http://" + req.headers.host + url_without_signature)
+                                     .digest('base64');
+        
+          if (signature != calculated_hash)  {
+            reject(); return;
+          }
+  
+          req.stopBuffering();
+          next();
+        }
+      });
+    });
+  }
+}
--- filecache.js
+++ filecache.js
@@ -6,46 +6,13 @@ var http = require('http'),
     
 var nStore = require('nstore'),
     connect = require('connect');
-
-function toArray(obj) {
-  var len = obj.length
-    , arr = new Array(len);
-  for (var i = 0; i < len; ++i) {
-    arr[i] = obj[i];
-  }
-  return arr;
-}
-
-http.IncomingMessage.prototype.startBuffering = function() {
-  this._eventBuffer = [];
-  this.on('data', function() {
-    this._eventBuffer.push(['data'].concat(toArray(arguments)));
-  });
-  this.on('end', function() {
-    this._eventBuffer.push(['end'].concat(toArray(arguments)));
-  })  
-}
-
-http.IncomingMessage.prototype.stopBuffering = function() {
-  that = this;
-  this.removeAllListeners('data');
-  this.removeAllListeners('end');
-  
-  setTimeout(function(array) {
-    return function() {
-      for (var i = 0; i < array.length; i++) {
-        console.log("replaying " + sys.inspect(array[i]));
-        that.emit.apply(that, array[i]);
-      }
-    }    
-  }(this._eventBuffer), 20);
-
-  this._eventBuffer = null;
-}
+    
+var auth = require('./authenticate')
+console.log(auth);
 
 var File = function(uuid) {
   var that = this;
-  var path = splittedUuid(uuid).concat(["data"]).join("/");
+  var path = splittedUuid(uuid).concat([uuid]).join("/");
   
   events.EventEmitter.call(this);
   this.path = path;
@@ -73,7 +40,6 @@ var File = function(uuid) {
 sys.inherits(File, events.EventEmitter);
 
 File.prototype.expired = function() {
-//  return false;
   return this.options.expires_at <= new Date().getTime() / 1000;
 }
 
@@ -86,7 +52,8 @@ File.prototype.streamTo = function(res) {
   if (!that.exists) throw new Error("File " + that.path + " does not exists");
 
   res.writeHead(200, {
-    'Content-Type': that.options.type
+    'Content-Type': that.options.type,
+    'Content-Disposition': that.options["content-disposition"]
   });
   
 
@@ -99,7 +66,6 @@ File.prototype.streamTo = function(res) {
     });
 
     readStream.on('end', function() {
-      console.log("end sent: " + that.options.size);
       if (that.sentBytes >= that.options.size) {
         res.end();
         fs.unwatchFile(that.path);
@@ -166,7 +132,7 @@ var saveToFile = function(req, res, next) {
   inCreatedDirectory(splittedUuid(uuid), function(path) {
     console.log("create file " + path);
     
-    dataStream = fs.createWriteStream(path + "data");
+    dataStream = fs.createWriteStream(path + uuid);
     dataStream.write(buffer);
     
     if (ended) {
@@ -226,54 +192,10 @@ var files = nStore.new('data/files', function() {
   if (started) {return;}
   
   connect.createServer(
-    authorized,
+    auth.authenticate(),
     connect.router(fileCache)
   ).listen(9876);
   
   console.log('Server running at http://127.0.0.1:9876/');
   started = true;
 });
-
-
-var mongo = require('mongodb');
-var crypto = require('crypto');
-
-var db = new mongo.Db('hoccer_development', new mongo.Server("127.0.0.1", 27017));
-db.open(function(){ console.log("open") });
-
-var authorized = function(req, res, next) {
-  var reject = function() {
-    res.writeHead(401);
-    res.end("authentification failed");
-  }
-  
-  req.startBuffering();
-  db.collection('accounts', function(err, collection) {
-    collection.findOne({"api_key": "37d4b750fc95012d14a7109add515cd4"}, {}, function(err, account) {
-      if (!account) {
-        reject(); return;
-      } else {
-        var result = req.url.match(/\&signature=(.*)$/);
-        if (result.length < 1) {
-          reject(); return;
-        }
-        
-        var signature = decodeURIComponent(result[1].toString());
-        var url_without_signature = req.url.match(/(.*)\&signature=.*$/)[1];
-        
-        console.log("http://" + req.headers.host + url_without_signature);
-        var calculated_hash = crypto.createHmac('sha1', account.shared_secret)
-                                   .update("http://" + req.headers.host + url_without_signature)
-                                   .digest('base64');
-        
-        
-        if (signature != calculated_hash)  {
-          reject(); return;
-        }
-  
-        req.stopBuffering();
-        next();
-      }
-    });
-  });
-}

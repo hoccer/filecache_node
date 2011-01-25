2821738454ba171cd048d1b5ca2af4b4929fb23e
Robert Palmer <hi@robertpalmer.de>
2011-01-25 17:29:55 +0100

    authentication middleware and hack for pausing streams

--- filecache.js
+++ filecache.js
@@ -7,6 +7,42 @@ var http = require('http'),
 var nStore = require('nstore'),
     connect = require('connect');
 
+function toArray(obj) {
+  var len = obj.length
+    , arr = new Array(len);
+  for (var i = 0; i < len; ++i) {
+    arr[i] = obj[i];
+  }
+  return arr;
+}
+
+http.IncomingMessage.prototype.startBuffering = function() {
+  this._eventBuffer = [];
+  this.on('data', function() {
+    this._eventBuffer.push(['data'].concat(toArray(arguments)));
+  });
+  this.on('end', function() {
+    this._eventBuffer.push(['end'].concat(toArray(arguments)));
+  })  
+}
+
+http.IncomingMessage.prototype.stopBuffering = function() {
+  that = this;
+  this.removeAllListeners('data');
+  this.removeAllListeners('end');
+  
+  setTimeout(function(array) {
+    return function() {
+      for (var i = 0; i < array.length; i++) {
+        console.log("replaying " + sys.inspect(array[i]));
+        that.emit.apply(that, array[i]);
+      }
+    }    
+  }(this._eventBuffer), 20);
+
+  this._eventBuffer = null;
+}
+
 var File = function(uuid) {
   var that = this;
   var path = splittedUuid(uuid).concat(["data"]).join("/");
@@ -104,9 +140,8 @@ var inCreatedDirectory = function(dicts, callback) {
 
 var splittedUuid = function(uuid) {
   var compact_uuid = uuid.replace(/[-\/]/g, "");
-  return [compact_uuid.slice(0, 4), compact_uuid.slice(4)];
+  return ["data", compact_uuid.slice(0, 4), compact_uuid.slice(4,8)];
 }
-
 
 var saveToFile = function(req, res, next) {
   var dataStream, ended = false;
@@ -140,6 +175,7 @@ var saveToFile = function(req, res, next) {
   });
   
   req.on('data', function(chunk) {
+    console.log("data");
     if (dataStream) {
       dataStream.write(chunk);
     } else {
@@ -166,7 +202,7 @@ var loadFile = function(req, res, next) {
       res.writeHead(404);
       res.end("file expired");
     } else {
-        _file.streamTo(res);
+      _file.streamTo(res);
     }
   });
 }
@@ -190,9 +226,54 @@ var files = nStore.new('data/files', function() {
   if (started) {return;}
   
   connect.createServer(
+    authorized,
     connect.router(fileCache)
   ).listen(9876);
   
   console.log('Server running at http://127.0.0.1:9876/');
   started = true;
 });
+
+
+var mongo = require('mongodb');
+var crypto = require('crypto');
+
+var db = new mongo.Db('hoccer_development', new mongo.Server("127.0.0.1", 27017));
+db.open(function(){ console.log("open") });
+
+var authorized = function(req, res, next) {
+  var reject = function() {
+    res.writeHead(401);
+    res.end("authentification failed");
+  }
+  
+  req.startBuffering();
+  db.collection('accounts', function(err, collection) {
+    collection.findOne({"api_key": "37d4b750fc95012d14a7109add515cd4"}, {}, function(err, account) {
+      if (!account) {
+        reject(); return;
+      } else {
+        var result = req.url.match(/\&signature=(.*)$/);
+        if (result.length < 1) {
+          reject(); return;
+        }
+        
+        var signature = decodeURIComponent(result[1].toString());
+        var url_without_signature = req.url.match(/(.*)\&signature=.*$/)[1];
+        
+        console.log("http://" + req.headers.host + url_without_signature);
+        var calculated_hash = crypto.createHmac('sha1', account.shared_secret)
+                                   .update("http://" + req.headers.host + url_without_signature)
+                                   .digest('base64');
+        
+        
+        if (signature != calculated_hash)  {
+          reject(); return;
+        }
+  
+        req.stopBuffering();
+        next();
+      }
+    });
+  });
+}
